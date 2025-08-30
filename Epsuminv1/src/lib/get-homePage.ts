e6a2a73e3c9d0889c7e5e968441b08dcb4938a6c
Bacze12 <brunocc.0205@gmail.com>
2025-08-29 20:26:46 -0400

    conexion a mision y vision funcionando

--- Epsuminv1/src/lib/get-homePage.ts
+++ Epsuminv1/src/lib/get-homePage.ts
@@ -1,6 +1,6 @@
 import { query } from "./strapi-api";
 
-export async function getHomeInfo() {
+export async function getQuienesSomos() {
   return query("home-page?populate=quienesSomos").then((res) => {
     if (res.data === null) {
       console.error(
@@ -10,5 +10,18 @@ export async function getHomeInfo() {
     }
     console.log(res.data.quienesSomos);
     return res.data.quienesSomos;
+  });
+}
+
+export async function getMisionVision() {
+  return query("home-page?populate=mision_vision").then((res) => {
+    if (res.data === null) {
+      console.error(
+        "No se encontró el registro para la página de inicio en Strapi."
+      );
+      return null;
+    }
+    console.log(res.data.mision_vision);
+    return res.data.mision_vision;
   });
 }
